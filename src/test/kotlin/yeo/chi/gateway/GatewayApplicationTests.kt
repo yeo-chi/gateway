d454ab2e08b1edf6531225910c51e5b6d885801c
yeo-chi <00www.black@gmail.com>
2023-12-11 17:34:40 +0900

    초기 설정

---
+++ src/test/kotlin/yeo/chi/gateway/GatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package yeo.chi.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
